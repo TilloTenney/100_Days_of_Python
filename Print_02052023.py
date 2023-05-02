845eb3dfba5cf91292c84b4fc745736736a6e866
TilloTenney <131945703+TilloTenney@users.noreply.github.com>
2023-05-02 14:53:38 +0530

    Comment_Update_Print_02052023

--- Print_02052023.py
+++ Print_02052023.py
@@ -1,4 +1,4 @@
-#Write your code below this line 👇
+#In this codw we can execute print function for multiline output.
 print("""Day 1 - Python Print Function
 The function is declared like this:
 print('what to print')
