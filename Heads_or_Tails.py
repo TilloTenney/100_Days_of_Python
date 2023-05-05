5e6ded9cfdf4a67f858071e32e89096fc2898f23
TilloTenney <131945703+TilloTenney@users.noreply.github.com>
2023-05-05 12:57:23 +0530

    Heads_or_Tails

---
+++ Heads_or_Tails.py
@@ -0,0 +1,7 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+coin = random.randint(0,1)
+print("Heads" if coin == 1 else "Tails")
